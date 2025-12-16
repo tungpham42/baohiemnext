b5633301a2bcca76cdc741fbacd3bf386576acc4
Tung Pham <tung.42@gmail.com>
2025-12-16 17:40:59 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "@/pages/Home";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Trang chủ",
+  title: "Trang chủ | Cổng Thông Tin Bảo Hiểm",
 };
 
 export default function HomePage() {
